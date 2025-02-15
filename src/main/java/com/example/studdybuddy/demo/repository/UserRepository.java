2af10d38291c8cbdc12f7c7b5e1258e5d3323ed3
sachaloeb <144175746+sachaloeb@users.noreply.github.com>
2025-02-15 14:00:52 +0100

    netlify set up

--- src/main/java/com/example/studdybuddy/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.studdybuddy.demo.repository;
-
-import com.example.studdybuddy.demo.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
